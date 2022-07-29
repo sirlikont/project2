b3aa4edc5e600007663891ffd2b50c1644546e95
Sirli Kont <sirli.kont@pipedrive.com>
2022-07-29 15:12:16 +0300

    data added

---
+++ index.js
@@ -0,0 +1,12 @@
+const Koa = require('koa');
+const app = new Koa();
+
+const bodyParser = require("koa-bodyparser");
+app.use(bodyParser());
+
+const router = require('./lib/routes/post-router');
+app.use(router.routes());
+
+app.listen(3000, () =>
+   console.log('Server is running at http://localhost:3000')
+);
